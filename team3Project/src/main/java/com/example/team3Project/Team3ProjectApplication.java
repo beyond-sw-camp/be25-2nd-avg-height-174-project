59e17589206f9ea8a113b07d22918e9294152893
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-02-27 10:36:12 +0900

    초반 spring 배치
    초반 뼈대

---
+++ team3Project/src/main/java/com/example/team3Project/Team3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.team3Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team3ProjectApplication.class, args);
+	}
+
+}
