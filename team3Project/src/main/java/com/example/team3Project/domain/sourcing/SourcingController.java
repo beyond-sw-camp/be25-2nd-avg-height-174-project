9d0e0f53129c0976927ce9ee6cf54a4017cd58b0
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-03-06 13:24:13 +0900

    mariaDB 연동 및 저장 완료.
    DB연동 성공 및 json 데이터 파싱 및 저장 완료

--- team3Project/src/main/java/com/example/team3Project/domain/sourcing/SourcingController.java
+++ team3Project/src/main/java/com/example/team3Project/domain/sourcing/SourcingController.java
@@ -1,6 +1,7 @@
 package com.example.team3Project.domain.sourcing;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/sourcing")
+@RequiredArgsConstructor
 public class SourcingController {
+
+    private final SourcingService sourcingService;
+    
 
     @GetMapping("/")
     public String mainHome() {
@@ -27,31 +34,32 @@ public class SourcingController {
     @PostMapping("/upload")
     public Map<String, Object> handleFileUpload(@RequestPart("file") 
                                     MultipartFile file,
-                                    @RequestPart("data") Map<String, Object> json
-                                    ) {
-        
+                                    @RequestPart("data") SourcingDTO sourcingDTO) 
+    {
         Map<String,Object> response = new HashMap<>();
-        Map<String,Object> dataPart = (Map<String,Object>) json.get("data");
 
-        // 파일이 비어있다면 json에 따로 넣기.
-        
+        // 파일이 비어있다면 에러 메세지 띄우기.
+
         if (file.isEmpty()) {
             response.put("status", "error");
             response.put("message", "파일을 선택해 주세요.");
             return response;
         }
         
-        System.out.println(dataPart);
-
-
-        if (dataPart != null) {
-            String title = (String) dataPart.get("title");
-            String original_price = String.valueOf(dataPart.get("original_price"));
-
-            System.out.println("상품명:  " + title);
-            System.out.println("가격:  " + original_price);
+        List<String> errors = sourcingService.validateSourcingData(sourcingDTO);
+        
+        // service에서 에러가 뭐라도 하나 있더라면, 그거 확인 시키기
+        if (!errors.isEmpty()) {
+            response.put("status", "error");
+            response.put("message", "데이터 검증 실패");
+            response.put("errors", errors);
+        }else {
+            response.put("status", "success");
+            response.put("receivedData", sourcingDTO);
         }
         
+        sourcingService.saveSourcingData(sourcingDTO);
+
         return response;
 
     }
