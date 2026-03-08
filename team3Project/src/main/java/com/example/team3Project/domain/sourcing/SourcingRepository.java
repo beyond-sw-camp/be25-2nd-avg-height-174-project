020451d25b243f0a3e07254b35aba224bc9cdddb
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-03-08 14:04:39 +0900

    Revert "mariaDB 연동 및 저장 완료."
    This reverts commit aa08975a3db0f652054516eec451ee5e0ce335e3.

--- team3Project/src/main/java/com/example/team3Project/domain/sourcing/SourcingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.team3Project.domain.sourcing;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SourcingRepository extends JpaRepository<Sourcing, Long>{
-
-}
