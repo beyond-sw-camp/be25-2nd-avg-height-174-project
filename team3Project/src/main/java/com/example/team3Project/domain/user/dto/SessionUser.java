d85736df7f2b82a230744221e4b1743564836328
wldusdus63 <wldusdus63@gmail.com>
2026-03-27 12:28:01 +0900

    users
    사용자 계정 관리

--- team3Project/src/main/java/com/example/team3Project/domain/user/dto/SessionUser.java
+++ team3Project/src/main/java/com/example/team3Project/domain/user/dto/SessionUser.java
@@ -6,11 +6,17 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Data // getter, setter, toString, equals, hashCode 자동 생성
+@NoArgsConstructor // 기본 생성자 자동 생성
+@AllArgsConstructor // 모든 필드를 파라미터로 받는 생성자 자동 생성
 public class SessionUser implements Serializable {
+
+    // 로그인한 사용자의 고유 ID
     private Long id;
+
+    // 로그인한 사용자의 아이디(username)
     private String username;
+
+    // 로그인한 사용자의 닉네임
     private String nickname;
 }
