b1c21b015257b501b283033e11f21e92e5502b5e
wldusdus63 <wldusdus63@gmail.com>
2026-04-03 18:21:20 +0900

    이름, 전화번호 추가

--- team3Project/src/main/java/com/example/team3Project/domain/user/dto/UserUpdateFormRequest.java
+++ team3Project/src/main/java/com/example/team3Project/domain/user/dto/UserUpdateFormRequest.java
@@ -14,6 +14,9 @@ public class UserUpdateFormRequest {
     @NotBlank(message = "닉네임을 입력해주세요.")
     private String nickname;
 
+    @NotBlank(message = "전화번호를 입력해주세요.")
+    private String phoneNumber;
+
     @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
