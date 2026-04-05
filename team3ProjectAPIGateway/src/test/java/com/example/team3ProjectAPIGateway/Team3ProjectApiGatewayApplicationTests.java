10571a5b6313bbd98a91d7b0d36200fa4f2bfd33
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-04-05 14:22:37 +0900

    APIGateway 기본 설정들.

---
+++ team3ProjectAPIGateway/src/test/java/com/example/team3ProjectAPIGateway/Team3ProjectApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team3ProjectAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3ProjectApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
